09a7b62994c893a56f190347a580c93e6bf9355b
Rebecca <rebeccamitchell88@gmail.com>
2017-03-30 11:22:16 -0400

    Added fun facts controller functionality and some fun facts!! FIREFLIY LARVAE EAT SNAILS!!!!!!

--- Assets/LL_Assets/Scripts/FunFactController.cs
+++ Assets/LL_Assets/Scripts/FunFactController.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FunFactController : MonoBehaviour
 {
+    private string[] facts;
+    private int numOfFacts = 5;
+
+    [SerializeField]
+    Text factsText;
+    [SerializeField]
+    float waitTime = 7.0f;
+
+
+    private void Awake()
+    {
+        facts = new string[numOfFacts];
+
+        facts[0] = "There are more than 2,000 species of fireflies";
+        facts[1] = "In some places at some times, fireflies synchronize their flashing";
+        facts[2] = "A fireflies light is the most efficient light in the world";
+        facts[3] = "Firefly larvae are carnivorous and particularly enjoy snails";
+        facts[4] = "Observing fireflies in your backyard can help scientists learn more about these insects and why they're dissapearing";
+
+    }
 
     // Use this for initialization
     void Start()
     {
+        factsText.text = facts[Random.Range(0, facts.Length)];
+
         StartCoroutine(Wait());
     }
 
@@ -19,7 +42,7 @@ public class FunFactController : MonoBehaviour
 
     private IEnumerator Wait()
     {
-        yield return new WaitForSeconds(7.0f);
+        yield return new WaitForSeconds(waitTime);
         this.GetComponent<SceneLoad>().LoadScene("MainMenu");
     }
 }
